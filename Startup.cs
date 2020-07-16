b9c8c1ff46b6ab84fabe6a5505a009691e7b2e68
xfiddyx <steven.woods88@hotmail.co.uk>
2020-07-16 10:08:06 +0100

    migration

--- Startup.cs
+++ Startup.cs
@@ -63,8 +63,9 @@ options.UseNpgsql(Configuration.GetConnectionString("NcNewsConnection")));
   }
 
   // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+  public void Configure(IApplicationBuilder app, IWebHostEnvironment env, NcNewsContext context)
   {
+   context.Database.Migrate();
    if (env.IsDevelopment())
    {
     app.UseDeveloperExceptionPage();
